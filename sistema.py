4ad1ec5a8470e918361110e9e006e53e2a13e222
paixaoalmeida <paixaowork5@gmail.com>
2023-05-10 06:34:30 -0300

    Criação da classe produto

--- sistema.py
+++ sistema.py
@@ -1,1 +1,10 @@
-#Projeto de um gerenciamento de estoque com integrações
+#Projeto de um gerenciamento de estoque com integraçõesc
+class Product():
+    def __init__(self,nome, categoria, fornecedor, preco_compra,preco_venda,quantidade_estoque):
+        self.nome = nome
+        self.categoria = categoria
+        self.fornecedor = fornecedor
+        self.preco_compra = preco_compra
+        self.preco_venda = preco_venda
+        self.quantidade_estoque = quantidade_estoque
+    
