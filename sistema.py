7dc9d04f6e91af08a9bbd61d4e85d4de42ea37e7
paixaoalmeida <paixaowork5@gmail.com>
2023-05-20 15:53:07 -0300

    Adição da querry ao postgreesql das infos dos produtos

--- sistema.py
+++ sistema.py
@@ -16,14 +16,21 @@ class Produto():
         pass
     
     def AddProductInfo(self):
-        list = [] #LIsta vazia que vai pegar os valores
-        arg = {'nome do produto': str, 'preço do produto': float, 'a quantidade': int} 
+        prod_list = [] #LIsta vazia que vai pegar os valores
+        arg = {'nome do produto:': str, 'preço do produto:': float, 'a quantidade:': int} 
         #Dicionário com o nome das perguntas e valores como tipos de dados 
     
         for nome, tipo in arg.items(): #dois argumentos no loop, key e valor
-            quest = tipo(input(f'Digite {nome}'))
-            list.append(quest) #A cada rodada do loop, acrescenta o resultado a lista
+            quest = tipo(input(f'Digite {nome} '))
+            prod_list.append(quest) #A cada rodada do loop, acrescenta o resultado a lista
         
+        try:
+            cursor.execute(f"""
+            INSERT INTO produtos (nome_produto,preco_produto,quantidade_produto)
+            VALUES(%s,%s,%s);
+            """, (prod_list[0], prod_list[1], prod_list[2]))
+        except:
+            print('Algo deu errado na Querrie!')
 produto = Produto() #Colocando a classe produto dentro de um objeto
 
 
@@ -31,6 +38,7 @@ produto = Produto() #Colocando a classe produto dentro de um objeto
 #Função Main - Parte principal
 def Main():
     produto.AddProductInfo()
+
 
 
 #Rodando a função Main do programa
