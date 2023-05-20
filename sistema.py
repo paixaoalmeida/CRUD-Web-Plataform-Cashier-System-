686be06c7240f3b1af4aabfcde81e17abd268e0c
paixaoalmeida <paixaowork5@gmail.com>
2023-05-19 21:24:24 -0300

    Inicio do corpo do código: Função para pegas os detalhes do produto e organização inicial

--- sistema.py
+++ sistema.py
@@ -2,19 +2,35 @@
 import sys #Lib to get the arguments
 import psycopg2 #Lib to connect to de PostgreeSQL database
 
+#CONNECTING TO DATABASE
+conn = psycopg2.connect(database="Sistema_caixa",
+                        host="localhost",
+                        user="postgres",
+                        password="teste",
+                        port="5432")
+cursor = conn.cursor()
+#--------------------------------------------#------------------------------------#------------------
+
 class Produto():
-    def __init__(self, preco, tipo):
+    def __init__(self,nome,preco,quantidade):
+        self.nome = nome
         self.preco = preco
-        self.tipo = tipo
+        self.quantidade = quantidade
 
-    def MostrarPreco(self):
-        return 'O preço é ' + self.preco
-    
-    
-    def MostrarTipo(self):
-        return 'O tipo é ' + self.tipo
-    
-produto = Produto(sys.argv[1], sys.argv[2])
 
-print(produto.MostrarPreco())
-print(produto.MostrarTipo())
+
+#Código em construção!!
+
+
+def Main():
+    list = []
+    arg = ['nome do produto', 'preço do produto','a quantidade']
+
+    for name in arg:
+        quest = input(f"Digite {name} ")
+        list.append(quest)
+    print(list)
+
+
+if __name__ == '__main__':
+    Main()
