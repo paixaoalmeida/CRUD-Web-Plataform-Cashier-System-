44801d3163b2613c3ad937769cb2c14e0ad8b228
paixaoalmeida <paixaowork5@gmail.com>
2023-06-04 11:15:08 -0300

    Docstrings added and designing the code in a clean way, professional, may i say

--- res/functions_system.py
+++ res/functions_system.py
@@ -1,25 +1,32 @@
-from src.sistema import *
+"""
+Library to make some random tests in the code
+and all kinda bullshit i need
+"""
+
 
 class SystemFunctions:
+    """
+    This function was made to take the user's input
+
+    It does add some key-values to a dictionary and iterates
+    over a for loop between the key and value
+
+    This way we can make all the questions we want just in one loop,
+    and ensure error handling (user cant type anything he wants and get away with it)
+
+    Finally, we do retun the indexes 0 and 1 of the user inputs, this indexes are
+    being used in the 'cliente.py' module in the add_client method
+    """
     def __init__(self):
         pass
 
-    def pegar_nomes(self):
+    def pegar_nomes():
         prod_list = []  # LIsta vazia que vai pegar os valores
         arg = {'nome do cliente:': str, 'CPF do cliente:': str}
-        # Dicionário com o nome das perguntas e valores como tipos de dados
 
-        for nome, tipo in arg.items():  # dois argumentos no loop, key e valor
+        for nome, tipo in arg.items():
             quest = tipo(input(f'Digite {nome} '))
-            prod_list.append(quest)  # A cada rodada do loop, acrescenta o resultado a lista
-        
-        return prod_list[0],prod_list[1] 
-        #Retornando o index da lista que pega o input dos usuários
+            prod_list.append(quest)
 
+        return prod_list[0], prod_list[1]
 
-    def ask_to_leave(self):
-        quest = str(input('Deseja sair? S/N')).upper().lower()
-        if quest == 'S':
-            exit(0)
-        elif quest == 'N':
-            pass
