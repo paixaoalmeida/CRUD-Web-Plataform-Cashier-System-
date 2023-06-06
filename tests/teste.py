1d679f6d8c1986c954e1a8bbd672df1d476a0ab6
paixaoalmeida <paixaowork5@gmail.com>
2023-06-06 10:39:29 -0300

    Testing the register_data_of_purchase method

--- tests/teste.py
+++ tests/teste.py
@@ -1,42 +1,5 @@
-#The purpose of this file is to make some tests in the code when i need to.
-import psycopg2
+from src.produto import RegisterProduct
 
-#from res.functions_system import *
+instance_class = RegisterProduct()
 
-class teste(Database):
-    def __init__(self):
-        super().__init__()
-        self.connect_to_database()
-
-    def register_purchase(self):
-        data_list = []  # LIsta vazia que vai pegar os valores
-        arg = {'ID do produto:': int, 'Quantidade comprada:': int,
-               'Valor da compra:': float,
-               'Nome do cliente:': str}
-        # Dicionário com o nome das perguntas e valores como tipos de dados
-
-        for nome, tipo in arg.items():  # dois argumentos no loop, key e valor
-            quest = tipo(input(f'Digite {nome} '))
-            data_list.append(quest)  # A cada rodada do loop, acrescent
-
-        try:
-            self.cursor.execute('''
-                UPDATE produtos
-                set quantidade_produto = quantidade_produto - %s
-                where id_product = %s
-            ''', (data_list[1], data_list[0]))
-            self.connection.commit()
-            self.connection.close()
-
-        except psycopg2.Error as erro_db:
-            print(f'Erro {erro_db}')
-
-
-
-
-
-
-
-testee = teste()
-
-testee.register_purchase()
+instance_class.register_data_of_purchase()
