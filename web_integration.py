41bbea2b90df34c5b60eca44661cf1629c8b90d1
paixaoalmeida <paixaowork5@gmail.com>
2023-06-24 19:39:07 -0300

    Added the db.connect in the code, so the *cursor* attribute will be available in the show_products function that goes to /produtos

--- web_integration.py
+++ web_integration.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask import render_template
 from flask import url_for
-from src.sistema import *
+from src.database.database import *
 
-
+db = Database()
 web = Flask(__name__)
+
+db.connect_to_database()
+#Always initialize connection to have the cursor attribute
 
 
 @web.route("/")
@@ -15,4 +18,5 @@ def hello():
 
 @web.route("/produtos")
 def show_produtos_get():
-    return product.show_products()
+    products = db.show_all_products()
+    return render_template('produtos.html', products=products)
