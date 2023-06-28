82a4bc78199348ed77de9b7430afeda69e7024ae
paixaoalmeida <paixaowork5@gmail.com>
2023-06-27 21:39:57 -0300

    Add a cliente through a HTML/CSS form

--- web_integration.py
+++ web_integration.py
@@ -1,16 +1,23 @@
 from flask import Flask
 from flask import render_template
 from flask import url_for
+from flask import request
+from flask import redirec
+from flask import abort
 from src.database.database import *
+from src.cliente import *
+
+pega_nome = SystemFunctions()
 
 db = Database()
 '''Initializing the Database module to use it
 in the code'''
 
+client = Cliente()
+
 web = Flask(__name__)
 '''Initializing Flask app in a object, so we can use it 
 in the code'''
-
 
 db.connect_to_database()
 '''We need to call this methode before running other stuff
@@ -20,9 +27,9 @@ once we call this method'''
 
 
 @web.route("/")
-def hello():
-    '''Main page of the system, here we declare the css stylesheet
-    and return the page.html template'''
+def main_page():
+    """Main page of the system, here we declare the css stylesheet
+    and return the page.html template"""
 
     url_for('static', filename='style.css')
     return render_template('page.html')
@@ -30,10 +37,23 @@ def hello():
 
 @web.route("/produtos")
 def show_produtos_get():
-    '''Here we show all the products that are available in the database
+    """Here we show all the products that are available in the database
     we put the show_all_products in the *products* object and return it
     when we declare the produtos.html template so we can use it in the
-    teplate with jinja2 template language'''
-    
+    teplate with jinja2 template language"""
+
     products = db.show_all_products()
     return render_template('produtos.html', products=products)
+
+
+@web.route("/registro", methods=['POST', 'GET'])
+def registro_cliente():
+    try:
+        if request.method == 'POST':
+            cliente = request.form['nome_cliente']
+            cpf = request.form['cpf_cliente']
+            client.add_client(cliente, cpf)
+    except:
+        abort(400)
+
+    return render_template('Registro_cliente.html')
